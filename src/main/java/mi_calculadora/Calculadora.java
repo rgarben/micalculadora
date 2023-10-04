c0c35b6ecd283f68315d297dc457fae7d6312619
rgarben <rgarciabenitez88@gmail.com>
2023-10-04 09:51:49 +0200

    mi calculadora segunda subida

--- src/main/java/mi_calculadora/Calculadora.java
+++ src/main/java/mi_calculadora/Calculadora.java
@@ -3,6 +3,10 @@ import java.lang.StringBuilder;
 
 public class Calculadora {
 
+	
+		/*
+		 * Este metodo va agregando al StringBuilder principal 
+		 * */
 		public static void add(String cadena, StringBuilder sb) {
 			
 			if(cadena!=null && sb!=null) {
@@ -11,6 +15,10 @@ public class Calculadora {
 			
 		}
 		
+		
+		/*
+		 * Este metodo limpia el Stringbuilder principal
+		 * */
 		public static void delete(String cadena, StringBuilder sb) {
 			
 			if(cadena.equals("clear")) {
@@ -19,41 +27,69 @@ public class Calculadora {
 			
 		}
 		
+		
+		/*
+		 * Este metodo filtra y calcula el total de la operacion
+		 * */
 		public static int calculate(StringBuilder sb) {
 			
 			int total=0;
 			String oPerador="";
 			StringBuilder num = new StringBuilder();
+						
 			
-			for(int i=0;i<sb.length();i++) {
-				
-				if(sb.charAt(i)=='+') {
-					if(oPerador.equals("+")) {
-						total+=Integer.valueOf(num.toString());
-					}else if(oPerador.equals("-")) {
-						total-=Integer.valueOf(num.toString());
+			try {
+				/*
+				 * Este for va agregando numeros dependiendo del operador
+				 * y va guardandolo en el total para finalmente mostrarlo
+				 * */
+				for(int i=0;i<sb.length();i++) {
+					
+					if(sb.charAt(i)=='+') {
+						if(oPerador.equals("+")) {
+							total+=Integer.valueOf(num.toString());
+						}else if(oPerador.equals("-")) {
+							total-=Integer.valueOf(num.toString());
+						}else {
+							total=Integer.valueOf(num.toString());
+						}
+						
+						oPerador="+";
+						num.setLength(0);
+						
+					}else if(sb.charAt(i)=='-') {
+						if(oPerador.equals("+")) {
+							total+=Integer.valueOf(num.toString());
+						}else if(oPerador.equals("-")) {
+							total-=Integer.valueOf(num.toString());
+						}else {
+							total=Integer.valueOf(num.toString());
+						}
+						
+						oPerador="-";
+						num.setLength(0);
+						
 					}else {
-						total=Integer.valueOf(num.toString());
+						num.append(sb.charAt(i));
 					}
-					
-					oPerador="+";
-					num.setLength(0);
-					
-				}else if(sb.charAt(i)=='-') {
-					if(oPerador.equals("+")) {
-						total+=Integer.valueOf(num.toString());
-					}else if(oPerador.equals("-")) {
-						total-=Integer.valueOf(num.toString());
-					}else {
-						total=Integer.valueOf(num.toString());
-					}
-					
-					oPerador="-";
-					num.setLength(0);
-					
-				}else {
-					num.append(sb.charAt(i));
 				}
+				
+				
+				/*
+				 * Aqui preguntamos cual era el ultimo operador para asignar el ultimo numero
+				 * */
+				if(oPerador.equals("+")) {
+					total+=Integer.valueOf(num.toString());
+				}else if(oPerador.equals("-")) {
+					total-=Integer.valueOf(num.toString());
+				}
+				
+			/*
+			 * Caturamos el exception y enviamos un mensaje de error
+			 * */
+			} catch (Exception e) {
+				sb.setLength(0);
+				sb.append("Error");
 			}
 			
 			return total;
